30708b80d73a70d8dbc62514436a3f3f9aa8cc76
Juliette Engel <juliette.engel@stokelp.com>
2025-09-10 03:58:46 +0200

    fix: add index to hitcount to boost perf

--- internal/model/stats.go
+++ internal/model/stats.go
@@ -10,7 +10,7 @@ type StatsEntry struct {
 	Limit     int       `gorm:"not null;uniqueIndex:idx_params" json:"-"`
 	Str1      string    `gorm:"not null;size:100;uniqueIndex:idx_params" json:"-"`
 	Str2      string    `gorm:"not null;size:100;uniqueIndex:idx_params" json:"-"`
-	HitCount  int64     `gorm:"not null;default:0" json:"hit_count"`
+	HitCount  int64     `gorm:"not null;default:0;index" json:"hit_count"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
